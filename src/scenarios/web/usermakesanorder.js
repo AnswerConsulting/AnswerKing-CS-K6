9f1b139ac0e55fb989eaadb7576627f07ae03a25
HarveyAnswer <harvey.sembhy@answerdigital.com>
2023-01-31 15:04:05 +0000

    changed a bit of code

--- src/scenarios/web/usermakesanorder.js
+++ src/scenarios/web/usermakesanorder.js
@@ -1,19 +1,19 @@
 import { post_orders } from "./interactions/postorders.js";
 import { get_products } from "./interactions/getproducts.js";
 import { SharedArray } from 'k6/data';
-import { Histogram } from "k6/metrics";
+import { Trend } from 'k6/metrics';
 
-let durations = new Histogram("loop_duration");
+let durations = new Trend("overall_iteration_time");
 
 const data = new SharedArray('test-data', function () {
     return JSON.parse(open('../fixtures/test_data.json'));
 });
 
 export function user_makes_an_order() {
-    let start = performance.now()
+    let start = Date.now()
     post_orders(data)
     get_products()
-    let end = performance.end()
+    let end = Date.now()
     let duration = end - start
     durations.add(duration)
 }
